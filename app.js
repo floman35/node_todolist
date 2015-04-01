521e1df0156a8240b026039c019892e6abf3e8be
florent balac <florent.balac@gmail.com>
2015-04-01 14:40:28 +0200

    Basic app completed, Add and Remove task implemented

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 var express = require('express');
+var session = require('cookie-session'); 
+var bodyParser = require('body-parser'); 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('hello world');
+/* On utilise les sessions */
+app.use(session({
+	secret:'todosecret'
+}))
+
+/* Init of the session cookie */
+app.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
+    }
+    next();
+})
+
+app.get('/todo', function(req, res) {
+	res.render('todo.ejs', {tasks: req.session.todolist});
+	console.dir(req.session.todolist)
+});
+
+app.post('/todo/add/', urlencodedParser, function(req, res) {
+	if (req.body.NewTask != '') {
+		console.dir(req.body)
+        req.session.todolist.push(req.body.NewTask);
+    }
+    res.redirect('/todo');
+})
+
+app.get('/todo/remove/:id', function(req, res) {
+	if (req.params.id != '') {
+        req.session.todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
 });
 
 app.listen(3000);
